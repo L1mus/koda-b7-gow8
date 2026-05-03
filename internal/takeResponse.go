372792bd6fa0eb0aea6e2dccd296048701d342ac
L1mus <ali.firefox@yahoo.com>
2026-05-03 17:39:09 +0700

    refactor : Create a Character struct tag to map the JSON keys provided by the Rick and Morty API. parse json using Unmarshal and NewDecoder/Decode

--- internal/takeResponse.go
+++ internal/takeResponse.go
@@ -1,12 +1,34 @@
 package internal
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
-func WebFetcher(url string) ([]byte,error){
+type Location struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Character struct {
+	ID       int        `json:"id"`
+	Name     string     `json:"name"`
+	Status   string     `json:"status"`
+	Species  string     `json:"species"`
+	Type     string     `json:"type"`
+	Gender   string     `json:"gender"`
+	Origin   Location   `json:"origin"`
+	Location Location   `json:"location"`
+	Image    string     `json:"image"`
+	Episode  []string   `json:"episode"`
+	URL      string     `json:"url"`
+	Created  string     `json:"created"` 
+}
+
+
+func WebFetcher(url string , chFetchData chan *Character) error{
 	res,err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error: ",err)
@@ -18,5 +40,21 @@ func WebFetcher(url string) ([]byte,error){
 		fmt.Println("Error : ", err)
 	}
 
-	return body,err
+	var character Character
+	err = json.Unmarshal(body,&character)
+	if err != nil{
+		println("Error ", err)
+	}
+
+	chFetchData <- &character
+	return nil
+}
+
+func DisplayResultFetchData(chFetchData chan *Character,exit chan bool) {
+	defer func(){
+		exit <- true
+	}()
+	for v := range chFetchData {
+		fmt.Printf("%+v\n", v)
+	}
 }
